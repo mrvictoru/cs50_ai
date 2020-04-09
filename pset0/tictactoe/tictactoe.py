1f487caa01d534175d2617ac52854243487bb4d0
Victor U <victoru@pop-os.localdomain>
2020-04-09 15:03:47 +0800

    finish sudo code

--- pset0/tictactoe/tictactoe.py
+++ pset0/tictactoe/tictactoe.py
@@ -49,6 +49,9 @@ def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
+
+    # look into the board directly and change the board for the given player
+
     raise NotImplementedError
 
 
@@ -56,6 +59,12 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
+
+    # check if any player fit winning siutation: 
+    # (1,1 1,2 1,3) or (2,1 2,2 2,3) or (3,1 3,2 3,3) or (1,1 2,1 3,1) or (1,2 2,2 3,3) or (1,3 2,3 3,3) or (1,1 2,2 3,3) or (1,3 2,2 3,1)
+
+
+
     raise NotImplementedError
 
 
@@ -63,6 +72,11 @@ def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
+
+    # check if winner return true
+
+    # else loop through board to check if no empty slot
+
     raise NotImplementedError
 
 
@@ -70,6 +84,9 @@ def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
+
+    # use the same logic as winner
+
     raise NotImplementedError
 
 
@@ -77,4 +94,17 @@ def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
+    # user player(board) to check turn
+    # if terminal true
+    #   return utility
+    # moves = available moves for game (actions)
+    # if X turn:
+    #   value = -infinity
+    #   for move in moves:
+    #       value = max(value,minimax(move))
+    # else:
+    #   value = infinty
+    #   for move in moves:
+    #       value = min(value,minimax(move))
+
     raise NotImplementedError
