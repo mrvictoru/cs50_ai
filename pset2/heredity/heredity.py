efab36c926f4c3f9896cc70e4f89bdbe0be32483
Victor U <victoru@pop-os.localdomain>
2020-05-17 17:45:39 +0800

    init update

--- pset2/heredity/heredity.py
+++ pset2/heredity/heredity.py
@@ -274,7 +274,19 @@ def update(probabilities, one_gene, two_genes, have_trait, p):
     Which value for each distribution is updated depends on whether
     the person is in `have_gene` and `have_trait`, respectively.
     """
-    raise NotImplementedError
+    for person in probabilities:
+        if person in one_gene:
+            probabilities[person]["gene"][1] += p
+        elif person in two_genes:
+            probabilities[person]["gene"][2] += p
+        else:
+            probabilities[person]["gene"][0] += p
+        
+        if person in have_trait:
+            probabilities[person]["trait"][True] += p
+        else:
+            probabilities[person]["Trait"][False] += p
+
 
 
 def normalize(probabilities):
