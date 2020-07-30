3a8364b7173517a7a44f425372589aa214493e74
Victor U <victoru@pop-os.localdomain>
2020-07-30 22:26:38 +0800

    try different parameter for nn

--- projects/2020/x/traffic/traffic.py
+++ projects/2020/x/traffic/traffic.py
@@ -93,15 +93,12 @@ def get_model():
         tf.keras.layers.MaxPool2D(pool_size=(2,2)),
         # Convolutional layer of 16 different filters
         tf.keras.layers.Conv2D(
-            16, (3,3), activation="relu", input_shape=(IMG_WIDTH,IMG_HEIGHT,3)
+            32, (3,3), activation="relu", input_shape=(IMG_WIDTH,IMG_HEIGHT,3)
         ),
         # Max-pooling layer, using 2x2 pool size
         tf.keras.layers.AveragePooling2D(pool_size=(2,2)),
         # Flatten units
         tf.keras.layers.Flatten(),
-        # Add a hidden layer with dropout
-        tf.keras.layers.Dense(512,activation="relu"),
-        tf.keras.layers.Dropout(0.5),
         # Add a hidden layer with dropout
         tf.keras.layers.Dense(512,activation="relu"),
         tf.keras.layers.Dropout(0.5),
