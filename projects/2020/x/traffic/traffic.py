fef23a9ddd341ca445ad4d19f6af27bca07288d4
Victor U <victoru@pop-os.localdomain>
2020-07-30 14:57:08 +0800

    config neural network to get good result

--- projects/2020/x/traffic/traffic.py
+++ projects/2020/x/traffic/traffic.py
@@ -97,7 +97,7 @@ def get_model():
             16, (3,3), activation="relu", input_shape=(IMG_WIDTH,IMG_HEIGHT,3)
         ),
         # Max-pooling layer, using 2x2 pool size
-        tf.keras.layers.MaxPool2D(pool_size=(2,2)),
+        tf.keras.layers.AveragePooling2D(pool_size=(2,2)),
         # Flatten units
         tf.keras.layers.Flatten(),
         # Add a hidden layer with dropout
