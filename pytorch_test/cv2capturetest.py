5344f78f78cae54cc8109d72face6d1caa1f266c
Victor <pingumacao@gmail.com>
2022-07-23 18:25:28 +0800

    init cv2test

---
+++ pytorch_test/cv2capturetest.py
@@ -0,0 +1,21 @@
+# to test opencv functionaility in python
+import numpy as np
+import cv2
+
+# create video capture object
+cap = cv2.VideoCapture(0)
+
+# read and display frame, press q to quit
+while True:
+    ret, frame = cap.read()
+    width = int(cap.get(3))
+    height = int(cap.get(4))
+
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+#release video capture object
+cap.release()
+cv2.destroyAllWindows()
