5a78d5fe10de23c5700115b770b5092d22f1bbef
Victor <pingumacao@gmail.com>
2022-09-03 15:09:33 +0800

    added time deltas to check time for each steps

--- pytorch_test/cv/detrdetect.py
+++ pytorch_test/cv/detrdetect.py
@@ -4,6 +4,7 @@ from transformers import DetrFeatureExtractor, DetrForObjectDetection
 from PIL import Image
 import torch
 import cv2
+import time
 
 model_name = "facebook/detr-resnet-50"
 
@@ -12,26 +13,35 @@ def cv2pil(img):
     return Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 
 def getobject(ogimg,threshold=0.9,test=False):
+    # get time delta
+    start = time.time()
 
     # get cuda device if available
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     imgDetect = ogimg.copy()
+    print("time delta 1 (copy image): ",time.time()-start)
+
     # convert input image to PIL image for model input
     pilimg = cv2pil(ogimg)
+    print("time delta 2 (cv2pil): ",time.time()-start)
+
     # create model and feature extractor
     feature_extractor = DetrFeatureExtractor.from_pretrained(model_name)
-
     model = DetrForObjectDetection.from_pretrained(model_name)
     model.to(device)
+    print("time delta 3 (create model and extractor): ",time.time()-start)
 
     # inferenceogimg
     inputs = feature_extractor(images = pilimg, return_tensors = "pt").to(device)
     outputs = model(**inputs)
+    print("time delta 4 (inference): ",time.time()-start)
 
     # convert outputs (bounding boxes and class logits) to COCO API
     target_sizes = torch.tensor([pilimg.size[::-1]]).to(device)
     results = feature_extractor.post_process(outputs, target_sizes)[0]
+    print("time delta 5 (convert to coco): ",time.time()-start)
+
     resultsboxes = []
 
     # draw bounding boxes on the original image
@@ -49,7 +59,9 @@ def getobject(ogimg,threshold=0.9,test=False):
             else:
                 cv2.rectangle(imgDetect, (box[0], box[1]), (box[2], box[3]), (0, 255, 0), 2)
                 cv2.putText(imgDetect, f"{model.config.id2label[label.item()]}", (box[0], box[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
-
+    
+    print("time delta 6 (draw bounding boxes): ",time.time()-start)
+    
     if test:
         return resultsboxes
     else:
