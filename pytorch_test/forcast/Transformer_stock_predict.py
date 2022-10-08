a9e2c50590ddc2863dde00f371367f14caa42a4a
Victor <pingumacao@gmail.com>
2022-10-08 17:26:36 +0800

    added function for getting data into desire seq

--- pytorch_test/forcast/Transformer_stock_predict.py
+++ pytorch_test/forcast/Transformer_stock_predict.py
@@ -1,12 +1,15 @@
-# Class for transformer model and related functions such as positional encoding and dataset
+# Class for transformer model and related functions such as positional encoding and dataset, etc.
 # see https://github.com/KasperGroesLudvigsen/influenza_transformer for reference
 from collections.abc import Sequence
 import torch
 import torch.nn as nn
 import math
 from torch import nn, Tensor
-from typing import Tuple
-
+from typing import Tuple, Optional, Any, Union, Callable
+import numpy as np
+import pandas as pd
+from pathlib import Path
+import yfinance as yf
 
 class PositionalEncoder(nn.Module):
     def __init__(self, d_model: int = 512, dropout: float = 0.1, max_len: int = 5000, batch_first: bool = False):
@@ -180,10 +183,11 @@ class TransformerDataset(torch.utils.data.Dataset):
     def get_src_trg(self,sequence:torch.Tensor, enc_seq_len: int, dec_seq_len: int, target_seq_len: int) -> Tuple[torch.tensor, torch.tensor, torch.tensor]:
         """
         Generate the src (encoder input), trg (decoder input) and trg_y (the target) sequences from a sequence.
+
         Args:
             sequence: tensor, a 1D tensor of length n where n = encoder input length + target sequence length
             enc_seq_len: int, length of input sequence given to the encoder layer of the transformer model.
-            target_seq_len: int, desired length of the target sequence (the one against which  the model output is compared)
+            target_seq_len: int, desired length of the target sequence (the one against which the model output is compared)
         Returns:
             src: tensor, 1D, input to the model
             trg: tensor, 1D, input to the model
@@ -200,3 +204,127 @@ class TransformerDataset(torch.utils.data.Dataset):
 
         return src, trg, trg_y.squeeze(-1) # change size from [batch_size, target_seq_len, num_features] to [batch_size, target_seq_len]
 
+# utility function to create the indices for the train, validation and test datasets
+def generate_square_subsequent_mask(dim1: int, dim2: int) -> Tensor:
+    """
+    Generates a mask for the sequence. The masked positions are filled with float('-inf').
+    Unmasked positions are filled with float(0.0).
+    see https://pytorch.org/tutorials/beginner/transformer_tutorial.html
+
+    Args:
+        dim1: int, the length of the first dimension of the mask, for both src and tgt masking, this must be target sequence length
+        dim2: int, the length of the second dimension of the mask, for src masking, this must be the encoder sequence length (length of input sequence to the model), for tgt masking, this must be the target sequence length
+    
+    Return:
+        Tensor of shape [dim1, dim2]
+    """
+    return torch.triu(torch.ones(dim1, dim2) * float('-inf'), diagonal=1)
+
+def get_indices_input_target(num_obs, input_len, step_size, forecast_horizon, target_len):
+    """
+    Produce all the start and end index position of all sub-sequences.
+    The indices will be used to split the data into sub-sequences on which the modle will be trained.
+
+    Args:
+        num_obs:    int, the number of observations in the dataset for which indices must be generated
+
+        input_len:  int, the length of the input sequence to the model (a sub-sequence of the entire data sequence)
+
+        step_size:  int, the step size between two consecutive sub-sequences
+                    if 1, the first sub-sequence will be indices 0-input_len, and the next will be 1-input_len.
+
+        forecast_horizon:   int, Number of index positions that the target is away from the last index position of the input sequence
+                            if horizon is 1, and the input sequence is data [0:10], the target will be data [11:target_len]
+
+        target_len: int, the length of the target sequence / output sequence
+    
+    Returns a tuple with 4 elements:
+    1) The index positions of the first element to be inlcuded in the input sequence
+    2) The index positions of the last element to be included in the input sequence
+    3) The index positions of the first element to be included in the target sequence
+    4) The index positions of the last element to be included in the target sequence
+    """
+    input_len = round(input_len) # just a precaution
+    start_position = 0
+    stop_position = num_obs - 1
+
+    inpseq_first_idx = start_position
+    inpseq_last_idx = inpseq_first_idx + input_len
+    target_first_idx = inpseq_last_idx + forecast_horizon
+    target_last_idx = target_first_idx + target_len
+    print("target_last_idx = {}".format(target_last_idx))
+    print("stop_position = {}".format(stop_position))
+    indices = []
+    while target_last_idx <= stop_position:
+        indices.append((inpseq_first_idx, inpseq_last_idx, target_first_idx, target_last_idx))
+        inpseq_first_idx += step_size
+        inpseq_last_idx += step_size
+        target_first_idx += inpseq_last_idx + forecast_horizon
+        target_last_idx += target_first_idx + target_len
+    return indices
+
+def get_indices_entire_sequence(data: pd.Dataframe, window_size: int, step_size: int) -> list:
+    """
+    Produce all the start and end index position of all sub-sequences.
+    The indices will be used to split the data into sub-sequences on which the modle will be trained.
+
+    Args:
+        data:   pd.Dataframe, the dataset for which indices must be generated
+
+        window_size:    int, the desired length of the sub-sequences. Should be (input_len + target_len)
+                        E.g. if you want the model to consider the past 100 time steps in order to preidct the future 50 time steps, window_size should be 100+50=150
+        
+        step_size:  int, the step size between two consecutive sub-sequences
+                    if 1, the first sub-sequence will be [0:window_size], and the next will be [1:window_size].
+
+    Returns:
+        list of tuples, each tuple contains the start and end index position of a sub-sequence.
+        These tuples should be used to slice the dataset into sub-sequences. These sub-sequences should then be passed into a function
+        that slices them into input and target sequences. 
+    """
+    stop_position = len(data)-1 # 1- because of 0 indexing
+
+    # Start the first sub-sequence at index position 0
+    subseq_first_idx = 0
+
+    subseq_last_idx = subseq_first_idx + window_size
+
+    indices = []
+
+    while subseq_last_idx <= stop_position:
+        indices.append((subseq_first_idx, subseq_last_idx))
+        subseq_first_idx += step_size
+        subseq_last_idx += step_size
+    return indices
+
+def to_numeric_and_downcast_data(df: pd.DataFrame):
+    """
+    Downcast columns in df to smallest possible version of it's existing data
+    type
+    """
+    fcols = df.select_dtypes('float').columns
+    
+    icols = df.select_dtypes('integer').columns
+
+    df[fcols] = df[fcols].apply(pd.to_numeric, downcast='float')
+    
+    df[icols] = df[icols].apply(pd.to_numeric, downcast='integer')
+
+    return df
+
+def get_stock_data_yf(stock_name, past_years, interval) -> pd.DataFrame:
+    """
+    Get stock data from yahoo finance
+
+    Args:
+        stock_name: str, the name of the stock to get data for
+        past_years: int, the number of years of data to get
+        interval:   str, the interval at which the data is collected. Valid values are 1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo
+                    see https://pypi.org/project/yfinance/ for more details
+    
+    Returns:
+        pd.DataFrame, the stock data
+    """
+    data = yf.download(stock_name, period=f'{past_years}y', interval=interval)
+    data = to_numeric_and_downcast_data(data)
+    return data
