317b03f06a8013decf6c3b44f9e943d1a2714496
Victor <33645782+mrvictoru@users.noreply.github.com>
2022-11-01 16:09:43 +0800

    Update getstock.py
    added new helper function

--- pytorch_test/reinforcementlearning/getstock.py
+++ pytorch_test/reinforcementlearning/getstock.py
@@ -18,7 +18,6 @@ def to_numeric_and_downcast_data(df: pd.DataFrame):
     return df
 
 # helper function that get stock data from a start date to now
-# include creating s
 def get_stock_data_yf(stock_name, past_years, interval) -> pd.DataFrame:
     """
     Get stock data from yahoo finance
@@ -53,4 +52,28 @@ def get_stock_data_yf_between(stock_name, start_date, end_date, interval) -> pd.
     """
     data = yf.download(stock_name, start=start_date, end=end_date, interval=interval)
     data = to_numeric_and_downcast_data(data)
+    return data
+
+# helper function that get stock data between two dates as well as the technical indicators
+def get_stock_data_yf_between_with_indicators(stock_name, start_date, end_date, interval, indicators) -> pd.DataFrame:
+    """
+    Get stock data from yahoo finance
+
+    Args:
+        stock_name: str, the name of the stock to get data for
+        start_date: str, the start date of the data to get
+        end_date:   str, the end date of the data to get
+        interval:   str, the interval at which the data is collected. Valid values are 1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo
+                    see https://pypi.org/project/yfinance/ for more details
+        indicators: list, the list of technical indicators to add to the data
+    
+    Returns:
+        pd.DataFrame, the stock data
+    """
+    data = yf.download(stock_name, start=start_date, end=end_date, interval=interval)
+    data = to_numeric_and_downcast_data(data)
+    df = ta.add_all_ta_features(data, open="Open", high="High", low="Low", close="Close", volume="Volume", fillna=True)
+    # add the indicators from df to data
+    for indicator in indicators:
+        data[indicator] = df[indicator]
     return data
