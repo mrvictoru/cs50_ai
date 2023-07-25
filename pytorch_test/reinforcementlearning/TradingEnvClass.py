317a86739857b0541a08702c8fc4c68a6053f8cc
Victor <33645782+mrvictoru@users.noreply.github.com>
2023-07-25 18:03:08 +0800

    Update TradingEnvClass.py
    change delay modifier

--- pytorch_test/reinforcementlearning/TradingEnvClass.py
+++ pytorch_test/reinforcementlearning/TradingEnvClass.py
@@ -168,9 +168,11 @@ class StockTradingEnv(gym.Env):
         self.action_history.append(action)
 
         # calculate reward based on the net worth/balance with a delay modifier. which bias towards having a higher balance towards the end of the episode
-        delay_modifier = (self.current_step / self.max_step)
+        # the modifier should be between 0.5 and 1, where toward the start of the episode it is closer to 0.5 and towards the end it is closer to 1
+        delay_modifier = 0.5 + 0.5 * (self.current_step / self.max_step)
+        # TODO: change the reward function so that agent can take into other factors
         reward = self.net_worth * delay_modifier
-        # reward = self.net_worth * delay_modifier 
+        
         # if net_worth is below 0, or current_step is greater than max_step, then environment terminates
         done = self.net_worth <= 0 or self.current_step >= self.max_step or self.balance <= 0
 
